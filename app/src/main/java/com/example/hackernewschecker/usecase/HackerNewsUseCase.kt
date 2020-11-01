7a5af4ae02b4cdbc61c2978147588b263f0b5a50
WadaAkira <28423189+WadaAkira@users.noreply.github.com>
2020-11-01 21:29:56 +0900

    Hacker News API と通信するユースケースの I/F を実装

---
+++ app/src/main/java/com/example/hackernewschecker/usecase/HackerNewsUseCase.kt
@@ -0,0 +1,24 @@
+package com.example.hackernewschecker.usecase
+
+import com.example.hackernewschecker.usecase.domain.News
+
+/**
+ * HackerNews API と通信するユースケース
+ */
+interface HackerNewsUseCase {
+    /**
+     * 最新の Hacker News の id のリストを取得する。<br>
+     * id は最大 30個取得する。
+     *
+     * @return Hacker News の id のリスト
+     */
+    suspend fun loadCurrentNewsIdList(): List<Int>
+
+    /**
+     * Hacker News の詳細情報を取得する
+     *
+     * @param newsId Hacker News の id
+     * @return id にひもづく記事の詳細
+     */
+    suspend fun loadNews(newsId: Int): News
+}
