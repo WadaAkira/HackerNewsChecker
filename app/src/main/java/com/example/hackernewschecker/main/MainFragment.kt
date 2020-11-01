7503ef0e2fca7b6c25906d57e6c5d0253c322e4b
WadaAkira <28423189+WadaAkira@users.noreply.github.com>
2020-11-01 21:03:41 +0900

    MainFragment のレイアウトを実装

--- app/src/main/java/com/example/hackernewschecker/main/MainFragment.kt
+++ app/src/main/java/com/example/hackernewschecker/main/MainFragment.kt
@@ -1,11 +1,19 @@
 package com.example.hackernewschecker.main
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.hackernewschecker.databinding.MainFragmentBinding
 
 /**
  * Hacker News API と通信し、結果を表示するフラグメント
  */
 class MainFragment: Fragment() {
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding ?: throw IllegalStateException("MainFragmentBinding is null.")
+
     companion object {
         /**
          * フラグメントを生成する
@@ -13,5 +21,19 @@ class MainFragment: Fragment() {
         fun newInstance(): MainFragment {
             return MainFragment()
         }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
