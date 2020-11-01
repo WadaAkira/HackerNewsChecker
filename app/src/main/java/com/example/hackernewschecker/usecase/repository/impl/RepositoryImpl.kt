8a17e1c27b7695e013921509e9a154c5313e2c6f
WadaAkira <28423189+WadaAkira@users.noreply.github.com>
2020-11-01 22:08:17 +0900

    okHttp3 利用の準備

--- app/src/main/java/com/example/hackernewschecker/usecase/repository/impl/RepositoryImpl.kt
+++ app/src/main/java/com/example/hackernewschecker/usecase/repository/impl/RepositoryImpl.kt
@@ -1,13 +1,49 @@
 package com.example.hackernewschecker.usecase.repository.impl
 
 import com.example.hackernewschecker.usecase.repository.Repository
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 class RepositoryImpl():Repository {
-    private val okHttpClient = OkHttpClient.Builder().build()
+    companion object {
+        private const val HACKER_NEWS_API_BASE_URL = "https://hacker-news.firebaseio.com/v0"
+        private const val TIMEOUT_SECOND = 30L
+    }
+
+    private var retrofit: Retrofit
+
+    init {
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        retrofit = Retrofit.Builder()
+            .baseUrl(HACKER_NEWS_API_BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(createOkHttpClient())
+            .build()
+    }
 
     override suspend fun loadCurrentNewsIdList(): List<Int> {
         // https://hacker-news.firebaseio.com/v0/topstories.json
         TODO("Not yet implemented")
+    }
+
+    // okHttpClient を作成する
+    private fun createOkHttpClient(): OkHttpClient {
+        // Todo リリース版ではログが出力されないように制御
+        return OkHttpClient()
+            .newBuilder()
+            .connectTimeout(TIMEOUT_SECOND, TimeUnit.SECONDS)
+            .readTimeout(TIMEOUT_SECOND, TimeUnit.SECONDS)
+            .addInterceptor(HttpLoggingInterceptor().also {
+                it.level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
     }
 }
