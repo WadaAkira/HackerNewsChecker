834ea48fbce48b148d3fe447672996dad0d4ace7
WadaAkira <28423189+WadaAkira@users.noreply.github.com>
2020-11-02 12:40:16 +0900

    フォーマッタを適用

--- app/src/main/java/com/example/hackernewschecker/usecase/repository/impl/RepositoryImpl.kt
+++ app/src/main/java/com/example/hackernewschecker/usecase/repository/impl/RepositoryImpl.kt
@@ -7,12 +7,11 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 
-class RepositoryImpl():Repository {
+class RepositoryImpl : Repository {
     companion object {
         private const val HACKER_NEWS_API_BASE_URL = "https://hacker-news.firebaseio.com/v0/"
         private const val TIMEOUT_SECOND = 30L
@@ -22,14 +21,14 @@ class RepositoryImpl():Repository {
 
     init {
         val moshi = Moshi.Builder()
-            .add(KotlinJsonAdapterFactory())
-            .build()
+                .add(KotlinJsonAdapterFactory())
+                .build()
 
         retrofit = Retrofit.Builder()
-            .baseUrl(HACKER_NEWS_API_BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .client(createOkHttpClient())
-            .build()
+                .baseUrl(HACKER_NEWS_API_BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .client(createOkHttpClient())
+                .build()
     }
 
     override suspend fun loadCurrentNewsIdList(): Call<List<Int>> {
@@ -40,12 +39,12 @@ class RepositoryImpl():Repository {
     private fun createOkHttpClient(): OkHttpClient {
         // Todo リリース版ではログが出力されないように制御
         return OkHttpClient()
-            .newBuilder()
-            .connectTimeout(TIMEOUT_SECOND, TimeUnit.SECONDS)
-            .readTimeout(TIMEOUT_SECOND, TimeUnit.SECONDS)
-            .addInterceptor(HttpLoggingInterceptor().also {
-                it.level = HttpLoggingInterceptor.Level.BODY
-            })
-            .build()
+                .newBuilder()
+                .connectTimeout(TIMEOUT_SECOND, TimeUnit.SECONDS)
+                .readTimeout(TIMEOUT_SECOND, TimeUnit.SECONDS)
+                .addInterceptor(HttpLoggingInterceptor().also {
+                    it.level = HttpLoggingInterceptor.Level.BODY
+                })
+                .build()
     }
 }
