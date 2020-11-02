2317dcb2ac9485a75f99d45363fdccbda204990f
WadaAkira <28423189+WadaAkira@users.noreply.github.com>
2020-11-02 15:21:17 +0900

    AS 4.1 向けの Spek プラグインが見当たらなく単体テストを実行できなかったため、単体テストを仮実装

--- app/src/main/java/com/example/hackernewschecker/di/Module.kt
+++ app/src/main/java/com/example/hackernewschecker/di/Module.kt
@@ -4,8 +4,15 @@ import com.example.hackernewschecker.usecase.HackerNewsUseCase
 import com.example.hackernewschecker.usecase.impl.HackerNewsUseCaseImpl
 import com.example.hackernewschecker.usecase.repository.Repository
 import com.example.hackernewschecker.usecase.repository.impl.RepositoryImpl
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 /**
@@ -13,15 +20,47 @@ import javax.inject.Singleton
  */
 @Module
 class Module {
+    companion object {
+        private const val HACKER_NEWS_API_BASE_URL = "https://hacker-news.firebaseio.com/v0/"
+        private const val TIMEOUT_SECOND = 30L
+    }
+
     @Singleton
     @Provides
-    fun provideRepository(): Repository {
-        return RepositoryImpl()
+    fun provideRetrofit(): Retrofit {
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(HACKER_NEWS_API_BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(createOkHttpClient())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRepository(retrofit: Retrofit): Repository {
+        return RepositoryImpl(retrofit)
     }
 
     @Singleton
     @Provides
     fun provideHackerNewsUseCase(repository: Repository): HackerNewsUseCase {
         return HackerNewsUseCaseImpl(repository)
+    }
+
+    // okHttpClient を作成する
+    private fun createOkHttpClient(): OkHttpClient {
+        // Todo リリース版ではログが出力されないように制御
+        return OkHttpClient()
+            .newBuilder()
+            .connectTimeout(TIMEOUT_SECOND, TimeUnit.SECONDS)
+            .readTimeout(TIMEOUT_SECOND, TimeUnit.SECONDS)
+            .addInterceptor(HttpLoggingInterceptor().also {
+                it.level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
     }
 }
--- app/src/main/java/com/example/hackernewschecker/usecase/repository/impl/RepositoryImpl.kt
+++ app/src/main/java/com/example/hackernewschecker/usecase/repository/impl/RepositoryImpl.kt
@@ -2,50 +2,12 @@ package com.example.hackernewschecker.usecase.repository.impl
 
 import com.example.hackernewschecker.usecase.repository.Repository
 import com.example.hackernewschecker.usecase.repository.api.HackerNewsApi
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
-class RepositoryImpl @Inject constructor() : Repository {
-    companion object {
-        private const val HACKER_NEWS_API_BASE_URL = "https://hacker-news.firebaseio.com/v0/"
-        private const val TIMEOUT_SECOND = 30L
-    }
-
-    private var retrofit: Retrofit
-
-    init {
-        val moshi = Moshi.Builder()
-            .add(KotlinJsonAdapterFactory())
-            .build()
-
-        retrofit = Retrofit.Builder()
-            .baseUrl(HACKER_NEWS_API_BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .client(createOkHttpClient())
-            .build()
-    }
-
+class RepositoryImpl @Inject constructor(val retrofit: Retrofit) : Repository {
     override suspend fun loadCurrentNewsIdList(): Call<List<Int>> {
         return retrofit.create(HackerNewsApi::class.java).loadCurrentNewsIdList()
-    }
-
-    // okHttpClient を作成する
-    private fun createOkHttpClient(): OkHttpClient {
-        // Todo リリース版ではログが出力されないように制御
-        return OkHttpClient()
-            .newBuilder()
-            .connectTimeout(TIMEOUT_SECOND, TimeUnit.SECONDS)
-            .readTimeout(TIMEOUT_SECOND, TimeUnit.SECONDS)
-            .addInterceptor(HttpLoggingInterceptor().also {
-                it.level = HttpLoggingInterceptor.Level.BODY
-            })
-            .build()
     }
 }
