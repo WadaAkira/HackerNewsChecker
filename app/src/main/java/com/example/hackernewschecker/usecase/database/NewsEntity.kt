d55a0d86629575945b096ad51c00f2628383f17e
WadaAkira <28423189+WadaAkira@users.noreply.github.com>
2020-11-03 19:18:29 +0900

    不要なアノテーションを除去

--- app/src/main/java/com/example/hackernewschecker/usecase/database/NewsEntity.kt
+++ app/src/main/java/com/example/hackernewschecker/usecase/database/NewsEntity.kt
@@ -1,6 +1,5 @@
 package com.example.hackernewschecker.usecase.database
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -10,11 +9,11 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "hacker_news")
 data class NewsEntity(
     @PrimaryKey(autoGenerate = false) val id: Int,
-    @ColumnInfo(name = "by") val by: String?,
-    @ColumnInfo(name = "descendants") val descendants: Int?,
-    @ColumnInfo(name = "score") val score: Int?,
-    @ColumnInfo(name = "time") val time: Int?,
-    @ColumnInfo(name = "title") val title: String?,
-    @ColumnInfo(name = "type") val type: String?,
-    @ColumnInfo(name = "url") val url: String?
+    val by: String?,
+    val descendants: Int?,
+    val score: Int?,
+    val time: Int?,
+    val title: String?,
+    val type: String?,
+    val url: String?
 )
