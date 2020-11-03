660975a1c39d3aa7cba1beeb979fdeda9f16f93d
WadaAkira <28423189+WadaAkira@users.noreply.github.com>
2020-11-03 23:09:44 +0900

    三点リーダタップ時にポップアップメニューを表示する処理を実装

--- app/src/main/java/com/example/hackernewschecker/HackerNewsCheckerActivity.kt
+++ app/src/main/java/com/example/hackernewschecker/HackerNewsCheckerActivity.kt
@@ -3,9 +3,10 @@ package com.example.hackernewschecker
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hackernewschecker.databinding.HackerNewsCheckerActivityBinding
 import com.example.hackernewschecker.main.MainFragment
@@ -41,7 +42,13 @@ class HackerNewsCheckerActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.reader -> Log.d("wada", "menu tapped.")
+            R.id.reader -> {
+                val menu = findViewById<View>(R.id.reader)
+                PopupMenu(this, menu).also {
+                    it.menuInflater.inflate(R.menu.hacker_news_checker_popup_menu, it.menu)
+                    it.show()
+                }
+            }
         }
         return false
     }
