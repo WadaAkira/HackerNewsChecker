d3a4f4049c504dbaccaefa16b28713f987607ed7
WadaAkira <28423189+WadaAkira@users.noreply.github.com>
2020-11-03 23:53:07 +0900

    履歴画面の Contract を作成

---
+++ app/src/main/java/com/example/hackernewschecker/history/HistoryContract.kt
@@ -0,0 +1,84 @@
+package com.example.hackernewschecker.history
+
+import android.net.Uri
+import com.example.hackernewschecker.usecase.domain.News
+
+/**
+ * 履歴画面の実装規約
+ */
+interface HistoryContract {
+    /**
+     * 履歴画面の仕様
+     */
+    interface Presenter {
+        /**
+         * プレゼンターをセットアップする
+         *
+         * @param view 表示規約
+         */
+        fun setup(view: View)
+
+        /**
+         * 履歴画面に表示する情報を読み込み表示する
+         */
+        fun loadPage()
+
+        /**
+         * Hacker News の詳細ページを開く
+         *
+         * @param news 開く Hacker News の情報
+         */
+        fun openNewsSite(news: News)
+
+        /**
+         * 履歴を削除する
+         *
+         * @param news 削除する履歴
+         */
+        fun deleteHistory(news: News)
+
+        /**
+         * プレゼンターの処理を解放する
+         */
+        fun stop()
+    }
+
+    /**
+     * 履歴画面の表示/画面遷移
+     */
+    interface View {
+        /**
+         * ローディングを表示する
+         */
+        fun showLoading()
+
+        /**
+         * ローディングを非表示化する
+         */
+        fun hideLoading()
+
+        /**
+         * Hacker News の表示履歴を表示する
+         *
+         * @param historyList 表示する Hacker News のリスト
+         */
+        fun showHistoryList(historyList: List<News>)
+
+        /**
+         * Hacker News の対象ページを表示する
+         *
+         * @param url 表示する Web サイトの URL
+         */
+        fun transitNewsSite(url: Uri)
+
+        /**
+         * まだ履歴がないことを表示する
+         */
+        fun showNoneHistory()
+
+        /**
+         * 履歴削除完了をトーストで通知する
+         */
+        fun showDeletedHistoryToast()
+    }
+}
