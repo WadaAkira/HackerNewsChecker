732574ef96fda0a98151caf09029c7b65201f568
WadaAkira <28423189+WadaAkira@users.noreply.github.com>
2020-11-03 23:25:25 +0900

    Popup のメニュータップ時にフラグメントを切り替える処理を実装

--- app/src/main/java/com/example/hackernewschecker/HackerNewsCheckerActivity.kt
+++ app/src/main/java/com/example/hackernewschecker/HackerNewsCheckerActivity.kt
@@ -3,11 +3,13 @@ package com.example.hackernewschecker
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.hackernewschecker.databinding.HackerNewsCheckerActivityBinding
 import com.example.hackernewschecker.main.MainFragment
 
@@ -28,10 +30,7 @@ class HackerNewsCheckerActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
 
         // MainFragment の設定
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.container, MainFragment.newInstance())
-            .commit()
+        switchFragment(MainFragment.newInstance())
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -42,13 +41,7 @@ class HackerNewsCheckerActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.reader -> {
-                val menu = findViewById<View>(R.id.reader)
-                PopupMenu(this, menu).also {
-                    it.menuInflater.inflate(R.menu.hacker_news_checker_popup_menu, it.menu)
-                    it.show()
-                }
-            }
+            R.id.reader -> createPopupMenu()
         }
         return false
     }
@@ -60,5 +53,35 @@ class HackerNewsCheckerActivity : AppCompatActivity() {
      */
     fun startWebBrowser(url: Uri) {
         startActivity(Intent(Intent.ACTION_VIEW, url))
+    }
+
+    // 以下 private メソッド
+
+    // Fragment を切り替える
+    private fun switchFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
+
+        // Todo 同じフラグメントを表示しようとしていないかチェックする処理を実装
+        Log.d("wada", supportFragmentManager.fragments.toString())
+    }
+
+    // Popup メニューを作成する
+    private fun createPopupMenu() {
+        val menu = findViewById<View>(R.id.reader)
+        PopupMenu(this, menu).also {
+            it.menuInflater.inflate(R.menu.hacker_news_checker_popup_menu, it.menu)
+            it.setOnMenuItemClickListener { menu ->
+                when (menu.itemId) {
+                    R.id.top -> switchFragment(MainFragment.newInstance())
+                    R.id.history -> switchFragment(MainFragment.newInstance())
+                    R.id.license -> switchFragment(MainFragment.newInstance())
+                }
+                true
+            }
+            it.show()
+        }
     }
 }
